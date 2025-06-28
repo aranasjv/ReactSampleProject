fc5aed35928b3a16664fd634311548b0e30028d7
JV Aranas (SBKNeo-IT-SBK) <01PH1666.John.A@ICT888.NET>
2025-06-28 21:47:26 +0800

    added department table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { Home } from './Home';
+import { Department } from './Department';
+import { Employee } from './Employee';
+import { BrowserRouter as Router, Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h3 className='d-flex justify-content-center m-3'>
+          React Test
+        </h3>
+
+        <nav className='navbar navbar-expand-sm bg-light navbar-dark justify-content-center'>
+          <ul className='navbar-nav'>
+            <li className='nav-item m-1'>
+              <NavLink className="btn btn-light btn-outline-primary" to="/home">
+                Home
+              </NavLink>
+            </li>
+            <li className='nav-item m-1'>
+              <NavLink className="btn btn-light btn-outline-primary" to="/department">
+                Department
+              </NavLink>
+            </li>
+            <li className='nav-item m-1'>
+              <NavLink className="btn btn-light btn-outline-primary" to="/employee">
+                Employee
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/department" element={<Department />} />
+          <Route path="/employee" element={<Employee />} />
+          <Route path="*" element={<h4>404 - Not Found</h4>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Employee.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Employee extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Employee Page</h2>
+      </div>
+    );
+  }
+}
